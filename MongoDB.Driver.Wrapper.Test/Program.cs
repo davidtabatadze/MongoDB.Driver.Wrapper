752333c7f84174bf87d98399cdda9885a3e266a7
David Tabatadze <tabatadzedat@gmail.com>
2019-09-23 18:36:35 +0400

    Creating solution infrastructure

---
+++ MongoDB.Driver.Wrapper.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MongoDB.Driver.Wrapper.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wrapper test...");
+            Console.WriteLine("all tests done...");
+            Console.ReadKey();
+        }
+    }
+}
